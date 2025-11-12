44c9da3827e1e4a7d55672222b8bef380a11205f
amolvishwasrao <vishwasrao.amol@gmail.com>
2025-11-12 13:45:22 +0530

    added get api response in app in data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ from snowflake.snowpark.functions import col
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
-
+#st.text(smoothiefroot_response.json())
+sf_dt= st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 # Write directly to the app
 st.title(f":cup_with_straw: Custom Smoothie Order Form :cup_with_straw:")
 name_on_order = st.text_input("Name on Smoothie")
